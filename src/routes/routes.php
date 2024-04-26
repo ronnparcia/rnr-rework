4357cc8670b005cc690ee626a79b62171656104d
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2024-04-26 20:09:24 +0800

    Add new route for category-based pagination in routes.php

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -7,3 +7,26 @@ $app->get('/', function ($request, $response, $args) {
         'data' => $data
     ]);
 });
+
+$app->get('/{category:films|shows|music|others}[/{page}]', 
+    function ($request, $response, $args) {
+        $category = $args['category']; // Get the category
+        $page = isset($args['page']) ? (int)$args['page'] : 1; // If page is not set, default to 1, else convert to integer
+
+        // Map category to tag ID
+        $tagMap = [
+            'films'  => 2147,
+            'shows'  => 2225,
+            'music'  => 2107,
+            'others' => 2226,
+        ];
+
+        $tag = $tagMap[$category]; // Get the tag ID based on the category
+        $perPage = 20; // Number of posts to retrieve per page
+
+        $data = getPosts($tag, $perPage, $page); // Get the posts
+
+        return $this->view->render($response, 'home.twig', [
+            'data' => $data
+        ]);
+});
